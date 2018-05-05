ce3e66e44d979cd7847a810f8962899a560d9b8a
Mijyuoon <mijyuoon@gmail.com>
2018-05-05 23:34:15 +0300

    Open "Add tags" window when passed a filename

--- Akyuu/App.xaml.cs
+++ Akyuu/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Akyuu.Data;
+using Akyuu.UI;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,6 +19,9 @@ namespace Akyuu {
         public Models.Config Config { get; private set; }
 
         private void App_Startup(object sender, StartupEventArgs e) {
+            string exepath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            Directory.SetCurrentDirectory(exepath);
+
             if(File.Exists(ConfigFilePath)) {
                 using(var fs = new FileStream(ConfigFilePath, FileMode.Open)) {
                     var ser = new DataContractJsonSerializer(typeof(Models.Config));
@@ -27,6 +33,25 @@ namespace Akyuu {
                     ScreenshotPath = Path.Combine(Directory.GetCurrentDirectory(), "Screenshots"),
                     ThumbnailSize = 64,
                 };
+            }
+
+            if(e.Args.Length > 0) {
+                string file = Path.GetFileName(e.Args[0]);
+
+                if(!File.Exists(Path.Combine(Config.ScreenshotPath, file))) {
+                    MessageBox.Show($"Invalid filename '{file}' provided, exiting.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Environment.Exit(-1);
+                }
+
+                var window = new AddTagsWindow(file);
+                if(window.ShowDialog() is true) {
+                    using(var ctx = AkyuuContext.Create()) {
+                        ctx.ScreenshotTags.AddRange(window.Tags);
+                        ctx.SaveChanges();
+                    }
+                }
+
+                Environment.Exit(0);
             }
         }
 
